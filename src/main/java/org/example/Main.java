8b87a90b8c5680f17cac42816bef3176d99d3737
Chengkang QU <12511309@mail.sustech.edu.cn>
2026-02-07 16:34:35 +0800

    Firt success: Create an openGL window!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,16 +2,51 @@ package org.example;
 
 //TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
 // 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
-public class Main {
-    public static void main(String[] args) {
-        //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
-        // 查看 IntelliJ IDEA 建议如何修正。
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
-            // 但您始终可以通过按 <shortcut actionId="ToggleLineBreakpoint"/> 添加更多断点。
-            System.out.println("i = " + i);
+import org.lwjgl.opengl.*;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+public class Main {
+    private long window;
+    public void run() {
+        init();
+        loop();
+        // 释放内存
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+
+    private void init() {
+        if (!glfwInit()) throw new IllegalStateException("无法初始化 GLFW");
+
+        // 配置窗口
+        window = glfwCreateWindow(800, 600, "My Minecraft (Notch Style)", NULL, NULL);
+        if (window == NULL) throw new RuntimeException("窗口创建失败");
+
+        // 设置当前 OpenGL 上下文
+        glfwMakeContextCurrent(window);
+        GL.createCapabilities(); // 这行非常重要，它连接了 Java 和 OpenGL
+
+        // 设置背景颜色（天空蓝）
+
+
+        glClearColor(0.5f, 0.8f, 1.0f, 0.0f);
+    }
+
+    private void loop() {
+        while (!glfwWindowShouldClose(window)) {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // 清屏
+
+            // 交换缓冲区，显示画面
+            glfwSwapBuffers(window);
+            glfwPollEvents();
         }
+    }
+
+    public static void main(String[] args) {
+        new Main().run();
     }
 }
